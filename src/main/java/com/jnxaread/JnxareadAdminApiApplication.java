bbf0a7fbc3884614db79521efc85cfa6d18828f4
未央 <hiveforwork@163.com>
2020-04-21 20:54:40 +0800

    初始化提交

---
+++ src/main/java/com/jnxaread/JnxareadAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.jnxaread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnxareadAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnxareadAdminApiApplication.class, args);
+    }
+
+}
