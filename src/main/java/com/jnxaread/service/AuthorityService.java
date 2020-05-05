e5a7814e23b13a4d961e1c76366a57585a1d6606
未央 <hiveforwork@163.com>
2020-05-05 19:03:49 +0800

    新增业务层和业务实现层

---
+++ src/main/java/com/jnxaread/service/AuthorityService.java
@@ -0,0 +1,8 @@
+package com.jnxaread.service;
+
+/**
+ * @author 未央
+ * @create 2020-05-05 19:01
+ */
+public interface AuthorityService extends BaseAuthorityService {
+}
---
+++ src/main/java/com/jnxaread/service/AuthorityServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jnxaread.service;
+
+import com.jnxaread.service.impl.BaseAuthorityServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 未央
+ * @create 2020-05-05 19:02
+ */
+@Service
+public class AuthorityServiceImpl extends BaseAuthorityServiceImpl implements AuthorityService {
+}
