294447ac2ad0d450f482fa1d288380b2645baa49
LiSong-ux <song.li@mycool.tv>
2022-03-15 10:48:22 +0800

    经过测试，Spring不支持cron周字段用数字表示，只能用字符串，比如：MON/TUE/WED等

--- src/main/java/com/jnxaread/timer/DataBackupTimerTask.java
+++ src/main/java/com/jnxaread/timer/DataBackupTimerTask.java
@@ -15,8 +15,9 @@ public class DataBackupTimerTask {
     @Resource
     private DataBackupService dataBackupService;
 
-    // 执行时间：每周二凌晨三点
-    @Scheduled(cron = " 0 0 3 ? * 3 ")
+    // 执行时间：每周三凌晨三点
+    // 这里有坑，经过实际测试，spring不支持cron的周字段用数字表示，只能用字符串
+    @Scheduled(cron = " 0 0 3 ? * WED ")
     public void dataBackup() {
         dataBackupService.dataBackup();
     }
